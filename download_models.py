c1ade441a2fc3eb3ccc0affd5adae5b5bd86fe53
Ytopoc <43822389+Ytopoc@users.noreply.github.com>
2024-09-01 18:28:16 +0300

    Yra?

--- download_models.py
+++ download_models.py
@@ -16,10 +16,38 @@ files_to_download = [
 save_dir = 'saved_model'
 os.makedirs(save_dir, exist_ok=True)
 
-for file in files_to_download:
-    url = f"https://drive.google.com/uc?id={file['id']}"
-    output_path = os.path.join(save_dir, file['name'])
-    print(f"Downloading {file['name']} from {url}...")
-    gdown.download(url, output_path, quiet=False)
+# for file in files_to_download:
+#     url = f"https://drive.google.com/uc?id={file['id']}"
+#     output_path = os.path.join(save_dir, file['name'])
+#     print(f"Downloading {file['name']} from {url}...")
+#     gdown.download(url, output_path, quiet=False)
+
+
+url = f"https://drive.google.com/uc?id={files_to_download[0]['id']}"
+output_path = os.path.join(save_dir, files_to_download[0]['name'])
+print(f"Downloading {files_to_download[0]['name']} from {url}...")
+gdown.download(url, output_path, quiet=False)
+
+url = f"https://drive.google.com/uc?id={files_to_download[1]['id']}"
+output_path = os.path.join(save_dir, files_to_download[1]['name'])
+print(f"Downloading {files_to_download[1]['name']} from {url}...")
+gdown.download(url, output_path, quiet=False)
+
+url = f"https://drive.google.com/uc?id={files_to_download[2]['id']}"
+output_path = os.path.join(save_dir, files_to_download[2]['name'])
+print(f"Downloading {files_to_download[2]['name']} from {url}...")
+gdown.download(url, output_path, quiet=False)
+
+url = f"https://drive.google.com/uc?id={files_to_download[3]['id']}"
+output_path = os.path.join(save_dir, files_to_download[3]['name'])
+print(f"Downloading {files_to_download[3]['name']} from {url}...")
+gdown.download(url, output_path, quiet=False)
+
+url = f"https://drive.google.com/uc?id={files_to_download[4]['id']}"
+output_path = os.path.join(save_dir, files_to_download[4]['name'])
+print(f"Downloading {files_to_download[4]['name']} from {url}...")
+gdown.download(url, output_path, quiet=False)
+
+
 
 print("All files successfully downloaded.")
